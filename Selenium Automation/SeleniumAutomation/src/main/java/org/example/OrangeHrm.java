18ca7f49eda1a639d3fbc599a3a085b3e99f568b
AsmithaAsmi <46952950+AsmithaAsmi@users.noreply.github.com>
2025-03-01 17:01:30 +0530

    Add files via upload

---
+++ Selenium Automation/SeleniumAutomation/src/main/java/org/example/OrangeHrm.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class OrangeHrm {
+
+    @FindBy(xpath = "//input[@name='username']")
+    WebElement userInput;
+
+    @FindBy(xpath = "//a/span[normalize-space()='Dashboard']")
+    WebElement dashboard;
+
+    WebDriver driver = new ChromeDriver();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        OrangeHrm orangeHrm = new OrangeHrm();
+        System.setProperty("webdriver.chrome.driver", "E:\\Selenium Automation\\SeleniumAutomation\\src\\test\\resources\\chromedriver.exe");
+        orangeHrm.driver.manage().window().maximize();
+        orangeHrm.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        orangeHrm.loginToOrangeHrm(readFromPropertyFile("UserName"), readFromPropertyFile("Password"));
+//        orangeHrm.quitDriver();
+    }
+    public void quitDriver(){
+        System.out.println("Quiting the driver");
+        driver.quit();
+    }
+
+    public void loginToOrangeHrm(String userName, String password) throws InterruptedException {
+        System.out.println("Login to the website");
+        Thread.sleep(5000);
+//        driver.findElement(By.xpath("//input[@name='username']")).click();
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys(userName);
+//        driver.findElement(By.xpath("//input[@name='password']")).click();
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    public static String readFromPropertyFile(String readText) throws IOException {
+        System.out.println("Reading from the file");
+        FileReader fileReader = new FileReader("E:\\Selenium Automation\\SeleniumAutomation\\src\\main\\resources\\config.properties");
+        Properties properties = new Properties();
+        properties.load(fileReader);
+        readText = properties.getProperty(readText);
+        return readText;
+    }
+}
